83873aa55b0d05d13b34105a0b84afacdfa00cc9
Szago <43099280+Szago@users.noreply.github.com>
2023-10-24 03:18:26 +0200

    LAB1

---
+++ Rodzinka.pl
@@ -0,0 +1,31 @@
+parent( pam, bob).       % Pam is a parent of Bob
+parent( tom, bob).
+parent( tom, liz).
+parent( bob, ann).
+parent( bob, pat).
+parent( pat, jim).
+
+female( pam).            % Pam is female
+female( liz).
+female( ann).
+female( pat).
+
+male( jim).
+male( tom).              % Tom is male
+male( bob).
+
+% Potomek
+child(X, Y) :- parent(Y, X).
+
+% Matka
+mother(X, Y) :- parent(X, Y), female(X).
+
+% Dziadek
+grandparent(X, Z) :- parent(X, Y), parent(Y, Z).
+
+% Siostra
+sister(X, Y) :- parent(Z, X), parent(Z, Y), female(X), X \= Y.
+
+% Przodek
+predecessor(X, Y) :- parent(X, Y).
+predecessor(X, Y) :- parent(X, Z), predecessor(Z, Y).
